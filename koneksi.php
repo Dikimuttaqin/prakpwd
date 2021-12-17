a8a1fd1eff1126243219e8e3cfab07126fc216a6
Dikimuttaqin <92155168+Dikimuttaqin@users.noreply.github.com>
2021-12-17 20:30:34 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,10 @@
 <?php
-$host="localhost"; //untuk hostnya 
-$username="root"; //untuk username nya
-$password=""; //untuk passwordnya, yaitu kosong
-$databasename="akademik"; //nama db nya, saya memakai akademik
-
-//sintak untuk mengkoneksikan ke database nya
+$host="localhost";
+$username="root";
+$password="";
+$databasename="akademik";
 $con=@mysqli_connect($host,$username,$password,$databasename);
 if (!$con) {
-//jika gagal, maka akan menampilkan errornya
  echo "Error: " . mysqli_connect_error();
 exit();
 }
